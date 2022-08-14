286a795e3fa21b8ba6c672c186bd5162cc36a7b6
Артемчук Алексей <elusiv@mail.ru>
2022-08-14 18:43:12 +0500

    fixed webpack v1.1 14.08.22

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,4 @@
-// TODO: write code here
 
-// comment this to pass build
-// const unusedVariable = 'variable';
-
-// for demonstration purpose only
 export default function demo(value) {
   return value;
 }
